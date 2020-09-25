5e3b5e929214104ac6333959408760803875f7af
Kenneth Achumahara <42631863+kachumahara@users.noreply.github.com>
2020-09-24 21:22:38 -0700

    created the route files

--- server.js
+++ server.js
@@ -11,4 +11,11 @@ const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => res.send("API running"));
 
+//Define routes
+
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/posts", require("./routes/api/posts"));
+
 app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
