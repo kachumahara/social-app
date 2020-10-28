97fcd247ef0a3a9a8bb4ba338e521f068abffd8f
Kenneth Achumahara <42631863+kachumahara@users.noreply.github.com>
2020-10-27 20:15:44 -0700

    added router,  auth login and regiter components

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,21 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => (
   <nav class="navbar bg-dark">
     <h1>
-      <a href="index.html">
-        <i class="fas fa-code"></i> ReactConnector
-      </a>
+      <Link to="/">
+        <i className="fas fa-code" /> ReactConnector
+      </Link>
     </h1>
     <ul>
       <li>
-        <a href="profiles.html">Developers</a>
+        <a href="!#">Developers</a>
       </li>
       <li>
-        <a href="register.html">Register</a>
+        <Link to="/register">Register</Link>
       </li>
       <li>
-        <a href="login.html">Login</a>
+        <Link to="/login">Login</Link>
       </li>
     </ul>
   </nav>
