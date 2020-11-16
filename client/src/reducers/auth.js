20cd80650e1f14f6407a8fb0ed773d5bdedb4ea5
Kenneth Achumahara <42631863+kachumahara@users.noreply.github.com>
2020-11-16 13:37:44 -0700

    added logout navbar functionalities

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -5,6 +5,7 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
+  LOGOUT,
 } from "../actions/types";
 
 // Setting up the first initial state to default
@@ -43,6 +44,7 @@ export default function (state = initialState, action) {
     case REGISTER_FAIL:
     case AUTH_ERROR:
     case LOGIN_FAIL:
+    case LOGOUT:
       localStorage.removeItem("token");
       return {
         ...state,
