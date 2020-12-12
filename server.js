0654c3e1d26a744050dbbbe9722d6b6ea06d54aa
Kenneth Achumahara <42631863+kachumahara@users.noreply.github.com>
2020-12-11 21:49:04 -0700

    deploy production

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -8,11 +9,9 @@ const app = express();
 connectDB();
 
 //Init Middleware
-app.use(express.json({ extended: true }));
+app.use(express.json({ extended: false }));
 
 const PORT = process.env.PORT || 5000;
-
-app.get("/", (req, res) => res.send("API running"));
 
 //Define routes
 
@@ -20,5 +19,16 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+// Serve static assests in production
+
+if (process.env.NODE_ENV == "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
